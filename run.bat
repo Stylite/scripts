df98d72938ef12b16220797549a46c487f610c58
William <26222884+Stylite@users.noreply.github.com>
2018-04-29 23:55:46 -0400

    Updating run.bat

--- run.bat
+++ run.bat
@@ -6,37 +6,45 @@ ECHO 1.Decrypt
 ECHO 2.Encrypt
 ECHO 3.Python
 ECHO 4.Generate Password
-ECHO 5.Exit
+ECHO 5.Memecrypt
+ECHO 6.Rotdecrypt
+ECHO 7.Exit
 ECHO ------------
 ECHO.
 
 
-set /p op=Type option:
+set /p op=Type option: 
 if "%op%"=="1" goto Decrypt
 if "%op%"=="2" goto Encrypt
 if "%op%"=="3" goto Python
 if "%op%"=="4" goto Generatepass
-if "%op%"=="5" goto Exit
-
+if "%op%"=="5" goto Memecrypt
+if "%op%"=="6" goto Rotdecrypt
+if "%op%"=="7" goto Exit
 
 :Decrypt
-python decrypt.py
+python scripts/decrypt.py
 goto Begin
-
 
 :Encrypt
-python encrypt.py
+python scripts/encrypt.py
 goto Begin
-
 
 :Python
 python
 goto Begin
 
 :Generatepass
-python generatepass.py
+python scriptsgeneratepass.py
 goto Begin
 
+:Memecrypt
+python scripts/memecrypt.py
+goto Begin
+
+:Rotdecrypt
+python scripts/rotdecrypt.py
+goto Begin
 
 :Exit
 ECHO Exiting...
